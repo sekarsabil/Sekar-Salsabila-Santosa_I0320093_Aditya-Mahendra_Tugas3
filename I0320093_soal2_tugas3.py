fe8f28c10e091772c1969508e5458a42f8f986d8
Sekar Salsabila <sekarsalsabilaaa@gmail.com>
2021-03-18 16:32:20 +0700

    Update

--- I0320093_soal2_tugas3.py
+++ I0320093_soal2_tugas3.py
@@ -1,14 +1,14 @@
 #Dictionary berisi hobi, sosial media, lagu kesukaan, makanan favorit
 dict = {'Nama': ['Sekar Salsabila Santosa'],
         'Hobi': ['Bernyanyi','Menonton Film','Mendengarkan Musik'],
-        'Sosial Media': ['Instagram', 'Line', 'Whatsapp'],
+        'Sosial Media': ['Instagram : @sekarsabill', 'Line : sekarsabil', 'snapchat : sekarsabill'],
         'Lagu Kesukaan': ['Like to be you','Mutual','Misfit'],
         'Makanan Favorit': ['Udang', 'Mie', 'Dimsum']}
 print(dict)
 
 #Mengubah Salah Satu Hobi dan Sosial Media
 dict['Hobi'][1]=('Belajar')
-dict['Sosial Media'][2]=('Twitter')
+dict['Sosial Media'][2]=('Twitter : eyow')
 
 #Menghapus 2 Makanan Favorit
 del dict['Makanan Favorit'][1:3]

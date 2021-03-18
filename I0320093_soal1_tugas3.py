f39ec541bbe5a7e2b6b630a78324e6cfc503b100
Sekar Salsabila <sekarsalsabilaaa@gmail.com>
2021-03-18 11:44:17 +0700

    Update

--- I0320093_soal1_tugas3.py
+++ I0320093_soal1_tugas3.py
@@ -1,1 +1,20 @@
+#Membuat list teman
+list_teman = ['zanet','yola','salma','diva','moris','titus','vincen','rana','tito','zafir']
 
+#Menampilkan isi list indeks nomor 4,6,7
+print("Nama Teman 4,6,7 : ", list_teman[3], ",", list_teman[5], ",", list_teman[6])
+
+#Mengganti nama teman pada list 3,5,9
+list_teman[2] = 'rara'
+list_teman[4] = 'rafli'
+list_teman[8] = 'stefany'
+
+#Menambah nama teman
+list_teman.append('issa')
+list_teman.append('gea')
+
+#Menampilkan list nama teman dengan perulangan
+print(list_teman*4)
+
+#Menampilkan panjang list
+print (len(list_teman))
--- I0320093_soal2_tugas3.py
+++ I0320093_soal2_tugas3.py
@@ -1,1 +1,14 @@
+#Dictionary berisi hobi, sosial media, lagu kesukaan, makanan favorit
+dict = {'Hobi': ['Bernyanyi','Menonton Film','Mendengarkan Musik'],'Sosial Media': ['Instagram', 'Line', 'Whatsapp'], 'Lagu Kesukaan': ['Like to be you','Mutual','Misfit'], 'Makanan Favorit': ['Udang', 'Mie', 'Dimsum']}
 
+#Mengubah Salah Satu Hobi dan Sosial Media
+dict['Hobi'][1]=('Belajar')
+dict['Sosial Media'][2]=('Twitter')
+
+#Menghapus 2 Makanan Favorit
+del dict['Makanan Favorit'][1:3]
+
+#Menambahkan Satu Hobi
+dict.update({"Hobi":"Jalan-jalan"})
+
+print(dict)
